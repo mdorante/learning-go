271a9b76e7f3fdb880018d51939b665878b0ae07
mdorante <mdorante10@gmail.com>
2021-01-27 15:06:12 -0300

    add newDeck func (similar to a constructor)

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,23 @@ import "fmt"
 // Create a new 'deck' type, which is like an "extension" of a slice of strings
 type deck []string
 
+// Creates a new deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	// using '_' for index vars, let's Go know that I don't need to use these vars
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // Create a "receiver function", which is like a method for all variables of type "deck"
 func (d deck) print() {
 	for i, card := range d {
